8a52e30ea68b16e0b63372cefe80c4dd349652d4
camihall <camihall@bu.edu>
2022-03-29 22:34:42 -0400

    links in nav go to new page

--- my_app/lib/NavBar.dart
+++ my_app/lib/NavBar.dart
@@ -1,4 +1,31 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:url_launcher/url_launcher.dart';
+import 'Progress.dart';
+import 'main.dart';
+import 'sos.dart';
+
+class SettingsRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings')
+      ),
+    );
+  }
+}
+
+class PoliciesRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Policies')
+      ),
+    );
+  }
+}
 
 class NavBar extends StatelessWidget {
   @override
@@ -18,34 +45,59 @@ class NavBar extends StatelessWidget {
           ListTile(
             leading: Icon(Icons.home),
             title: Text('Home'),
-            onTap: () => null,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Welcome!')),
+              );
+            }
           ),
           ListTile(
             leading: Icon(Icons.person),
             title: Text('Progress'),
-            onTap: () => null,
+            onTap: () {  
+            Navigator.push(  
+              context,  
+              MaterialPageRoute(builder: (context) => ProgressRoute()),  
+            );  
+          },
           ),
           ListTile(
             leading: Icon(Icons.notifications),
             title: Text('SOS'),
-            onTap: () => null,
+            onTap: () {
+              Navigator.push(  
+              context,  
+              MaterialPageRoute(builder: (context) => SOSRoute()),  
+            );  
+          },
           ),
           Divider(),
           ListTile(
             leading: Icon(Icons.settings),
             title: Text('Settings'),
-            onTap: () => null,
+            onTap: () {
+              Navigator.push(  
+              context,  
+              MaterialPageRoute(builder: (context) => SettingsRoute()),  
+              );  
+            },
           ),
           ListTile(
             leading: Icon(Icons.description),
             title: Text('Policies'),
-            onTap: () => null,
+            onTap: () {
+              Navigator.push(  
+              context,  
+              MaterialPageRoute(builder: (context) => PoliciesRoute()),  
+              );  
+            },
           ),
           Divider(),
           ListTile(
             title: Text('Exit'),
             leading: Icon(Icons.exit_to_app),
-            onTap: () => null,
+            onTap: () => null, //launch('https://www.google.com/'),
           ),
         ],
       ),
---
+++ my_app/lib/progress.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProgressRoute extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Progress')
+      ),
+    );
+  }
+}
