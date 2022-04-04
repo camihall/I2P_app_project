692ca729de0f9b8557f154868da00100b4f03a77
ShapiroJoel <joelis@bu.edu>
2022-04-04 17:07:37 -0400

    Exercises acting wack

--- my_app/lib/views/exercises.dart
+++ my_app/lib/views/exercises.dart
@@ -7,17 +7,29 @@ class exercises extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       title: 'Exercises',
-      home: Scaffold(
-          appBar: AppBar(
-            title: const Text('New Page'),
-            backgroundColor: new Color(0xFF0B3F24),
-          ),
-          body: exerciseAll()),
+      home: Scaffold(body: exerciseAll()),
     );
   }
 }
 
 class exerciseAll extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            backgroundColor: Colors.white,
+            body: Center(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[exerciseBox("Exercise 1")]))));
+    throw UnimplementedError();
+  }
+}
+
+class exerciseBox extends StatelessWidget {
+  exerciseBox(this.exerciseName);
+  String exerciseName;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,27 +50,13 @@ class exerciseAll extends StatelessWidget {
                               BorderRadius.all(Radius.circular(10.0))),
                       child: new Center(
                         child: new Text(
-                          "Exercise #1",
+                          exerciseName,
                           style: TextStyle(color: Colors.white, fontSize: 22),
                           textAlign: TextAlign.center,
                         ),
                       )),
                 ),
-                Container(
-                    height: 150.0,
-                    width: 300.0,
-                    decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.all(Radius.circular(10.0))),
-                    child: new Center(
-                      child: new Text(
-                        "Exercise #2",
-                        style: TextStyle(color: Colors.white, fontSize: 22),
-                        textAlign: TextAlign.center,
-                      ),
-                    )),
               ],
             ))));
-    throw UnimplementedError();
   }
 }
