32aa42a81485ee0e6426d07e78962f1d889443ee
Quang Nguyen <nddq@bu.edu>
2022-04-04 15:37:54 -0400

    fix conflict

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:my_app/routes/route_guard.dart';
 import 'package:redux/redux.dart';
 import 'package:my_app/routes/router.gr.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'state/appState.dart';
-import 'NavBar.dart';
-import 'home.dart';
 void main() {
   // Create your store as a final variable in the main function or inside a
   // State object. This works better with Hot Reload than creating it directly
@@ -16,33 +13,6 @@ void main() {
     title: 'I2P App',
     store: store,
   ));
-
-
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'New Page',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.green,
-      ),
-      home: const MyHomePage(title: 'New Page'),
-    );
-  }
-
 }
 
 class App extends StatefulWidget {
@@ -63,78 +33,12 @@ class _AppState extends State<App> {
   late final _appRouter = AppRouter(routeGuard: RouteGuard());
   @override
   Widget build(BuildContext context) {
-
     return StoreProvider<AppState>(
       store: store,
       child: MaterialApp.router(
           title: widget.title,
           routeInformationParser: _appRouter.defaultRouteParser(),
           routerDelegate: _appRouter.delegate()),
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to New Page, '
-              '$_username, '
-              'a tool to help the process of disengagement.',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Text(
-              '$_username',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Enter your username',
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          Navigator.push(  
-              context,  
-              MaterialPageRoute(builder: (context) => Home()),  
-            );  
-        },
-        label: const Text('Confirm'),
-        icon: const Icon(Icons.thumb_up),
-        backgroundColor: Colors.green,
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      drawer: NavBar(),
-
     );
   }
 }
