680c7fc0a32ca4655536214784a8ef6b69db758b
ShapiroJoel <joelis@bu.edu>
2022-04-05 16:14:26 -0400

    Multiple exercises

--- my_app/lib/views/exercises.dart
+++ my_app/lib/views/exercises.dart
@@ -21,8 +21,26 @@ class exerciseAll extends StatelessWidget {
             body: Center(
                 child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[exerciseBox("Exercise 1")]))));
-    throw UnimplementedError();
+                    children: [
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new exerciseBox("Exercise 1"),
+                        new exerciseBox("Exercise 2"),
+                      ]),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new exerciseBox("Exercise 3"),
+                        new exerciseBox("Exercise 4"),
+                      ]),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        new exerciseBox("Exercise 5"),
+                        new exerciseBox("Exercise 6"),
+                      ])
+                ]))));
   }
 }
 
@@ -30,33 +48,22 @@ class exerciseBox extends StatelessWidget {
   exerciseBox(this.exerciseName);
   String exerciseName;
 
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-                child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Container(
-                  height: 150.0,
-                  width: 300.0,
-                  color: Colors.transparent,
-                  child: Container(
-                      decoration: BoxDecoration(
-                          color: Colors.green,
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      child: new Center(
-                        child: new Text(
-                          exerciseName,
-                          style: TextStyle(color: Colors.white, fontSize: 22),
-                          textAlign: TextAlign.center,
-                        ),
-                      )),
-                ),
-              ],
-            ))));
+    return Container(
+      height: MediaQuery.of(context).size.height * .25,
+      width: MediaQuery.of(context).size.width * .20,
+      color: Colors.transparent,
+      child: Container(
+          decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.all(Radius.circular(10.0))),
+          child: new Center(
+            child: new Text(
+              exerciseName,
+              style: TextStyle(color: Colors.white, fontSize: 22),
+              textAlign: TextAlign.center,
+            ),
+          )),
+    );
   }
 }
