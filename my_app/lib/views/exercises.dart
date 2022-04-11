4b9ffd21a2a0828c8cadbdf5b4d08d9d75ba6c60
ShapiroJoel <joelis@bu.edu>
2022-04-11 17:04:39 -0400

    Exercises appearing

--- my_app/lib/views/exercises.dart
+++ my_app/lib/views/exercises.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/components/SideMenu.dart';
+import 'package:provider/provider.dart';
+import '../components/DashboardHeader.dart';
+import '../components/MenuController.dart';
+import '../responsive.dart';
 import 'NavBar.dart';
 
 class exercises extends StatelessWidget {
@@ -10,9 +14,82 @@ class exercises extends StatelessWidget {
       theme: ThemeData.light(),
       title: 'Exercises',
       home: Scaffold(
-          body: exerciseAll(),
-          appBar: AppBar(title: Text("Exercises")),
-          drawer: SideMenu()),
+        body: SafeArea(
+            child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              const Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
+            Expanded(
+                // It takes 5/6 part of the screen
+                flex: 5,
+                child: SafeArea(
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CustomPaint(
+                          painter: DashboardHeader(),
+                          child: SizedBox(
+                              height: 350,
+                              child: Column(
+                                children: [
+                                  const SizedBox(height: 100),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const SizedBox(width: 20),
+                                      if (!Responsive.isDesktop(context))
+                                        IconButton(
+                                          icon: const Icon(Icons.menu),
+                                          iconSize: 40,
+                                          onPressed: context
+                                              .read<MenuController>()
+                                              .controlMenu,
+                                        ),
+                                      const SizedBox(width: 30),
+                                      const Text("Exercises",
+                                          style: TextStyle(
+                                              color: Color(0xff0B3F24),
+                                              fontWeight: FontWeight.bold,
+                                              fontSize: 40)),
+                                    ],
+                                  ),
+                                  Row(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const SizedBox(width: 50),
+                                      if (!Responsive.isDesktop(context))
+                                        const SizedBox(width: 55),
+                                      const Text(
+                                          "You have 2 exercises to complete today",
+                                          style: TextStyle(
+                                              color: Color(0xff0B3F24),
+                                              fontWeight: FontWeight.normal,
+                                              fontSize: 20)),
+                                    ],
+                                  )
+                                ],
+                              )),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(left: 50, right: 50),
+                          child: exerciseAll(),
+                        )
+                      ],
+                    ),
+                  ),
+                ))
+          ],
+        )),
+      ),
     );
   }
 }
@@ -20,32 +97,26 @@ class exercises extends StatelessWidget {
 class exerciseAll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            body: Center(
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        new exerciseBox("Exercise 1"),
-                        new exerciseBox("Exercise 2"),
-                      ]),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        new exerciseBox("Exercise 3"),
-                        new exerciseBox("Exercise 4"),
-                      ]),
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        new exerciseBox("Exercise 5"),
-                        new exerciseBox("Exercise 6"),
-                      ])
-                ]))));
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new exerciseBox("Exercise 1"),
+            new exerciseBox("Exercise 2"),
+          ]),
+      Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new exerciseBox("Exercise 3"),
+            new exerciseBox("Exercise 4"),
+          ]),
+      Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            new exerciseBox("Exercise 5"),
+            new exerciseBox("Exercise 6"),
+          ])
+    ]);
   }
 }
 
