ac8003a2620add905b9758a2e7958fc4582eea89
Xinyue Ma <xinyuem7@bu.edu>
2022-04-20 12:35:16 -0400

    changed back dashboard

--- my_app/lib/views/dashboard.dart
+++ my_app/lib/views/dashboard.dart
@@ -4,7 +4,6 @@ import 'package:my_app/components/SideMenu.dart';
 import 'package:my_app/responsive.dart';
 
 import 'package:my_app/state/appState.dart';
-import 'package:my_app/views/progress.dart';
 import 'package:provider/provider.dart';
 import 'package:redux/redux.dart';
 import 'package:flutter_redux/flutter_redux.dart';
@@ -58,90 +57,80 @@ class _DashboardState extends State<Dashboard> {
             drawer: const SideMenu(),
             body: SafeArea(
                 child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // We want this side menu only for large screen
-                if (Responsive.isDesktop(context))
-                  const Expanded(
-                    // default flex = 1
-                    // and it takes 1/6 part of the screen
-                    child: SideMenu(),
-                  ),
-                Expanded(
-                    // It takes 5/6 part of the screen
-                    flex: 5,
-                    child: SafeArea(
-                      child: SingleChildScrollView(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            CustomPaint(
-                              painter: DashboardHeader(),
-                              child: SizedBox(
-                                  height: 350,
-                                  child: Column(
-                                    children: [
-                                      const SizedBox(height: 100),
-                                      Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          const SizedBox(width: 20),
-                                          if (!Responsive.isDesktop(context))
-                                            IconButton(
-                                              icon: const Icon(Icons.menu),
-                                              iconSize: 40,
-                                              onPressed: context
-                                                  .read<MenuController>()
-                                                  .controlMenu,
-                                            ),
-                                          const SizedBox(width: 30),
-                                          ElevatedButton(
-                                            child: const Text('Open route'),
-                                            onPressed: () {
-                                              Navigator.push(
-                                                context,
-                                                MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        const ProgressRoute()),
-                                              );
-                                            },
-                                          ),
-                                          const Text("Welcome",
-                                              style: TextStyle(
-                                                  color: Color(0xff0B3F24),
-                                                  fontWeight: FontWeight.bold,
-                                                  fontSize: 40)),
-                                        ],
-                                      ),
-                                      Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          const SizedBox(width: 50),
-                                          if (!Responsive.isDesktop(context))
-                                            const SizedBox(width: 55),
-                                          const Text("You have 2 tasks today",
-                                              style: TextStyle(
-                                                  color: Color(0xff0B3F24),
-                                                  fontWeight: FontWeight.normal,
-                                                  fontSize: 20)),
-                                        ],
-                                      )
-                                    ],
-                                  )),
-                            ),
-                            Container(
-                              padding:
-                                  const EdgeInsets.only(left: 50, right: 50),
-                              child: const DashboardOptions(),
-                            )
-                          ],
-                        ),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // We want this side menu only for large screen
+                    if (Responsive.isDesktop(context))
+                      const Expanded(
+                        // default flex = 1
+                        // and it takes 1/6 part of the screen
+                        child: SideMenu(),
                       ),
-                    ))
-              ],
-            )),
+                    Expanded(
+                        // It takes 5/6 part of the screen
+                        flex: 5,
+                        child: SafeArea(
+                          child: SingleChildScrollView(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                CustomPaint(
+                                  painter: DashboardHeader(),
+                                  child: SizedBox(
+                                      height: 350,
+                                      child: Column(
+                                        children: [
+                                          const SizedBox(height: 100),
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              const SizedBox(width: 20),
+                                              if (!Responsive.isDesktop(context))
+                                                IconButton(
+                                                  icon: const Icon(Icons.menu),
+                                                  iconSize: 40,
+                                                  onPressed: context
+                                                      .read<MenuController>()
+                                                      .controlMenu,
+                                                ),
+                                              const SizedBox(width: 30),
+                                              const Text("Welcome",
+                                                  style: TextStyle(
+                                                      color: Color(0xff0B3F24),
+                                                      fontWeight: FontWeight.bold,
+                                                      fontSize: 40)),
+                                            ],
+                                          ),
+                                          Row(
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.center,
+                                            children: [
+                                              const SizedBox(width: 50),
+                                              if (!Responsive.isDesktop(context))
+                                                const SizedBox(width: 55),
+                                              const Text("You have 2 tasks today",
+                                                  style: TextStyle(
+                                                      color: Color(0xff0B3F24),
+                                                      fontWeight:
+                                                          FontWeight.normal,
+                                                      fontSize: 20)),
+                                            ],
+                                          )
+                                        ],
+                                      )),
+                                ),
+                                Container(
+                                  padding:
+                                      const EdgeInsets.only(left: 50, right: 50),
+                                  child: const DashboardOptions(),
+                                )
+                              ],
+                            ),
+                          ),
+                        ))
+                  ],
+                )),
           );
         });
   }
