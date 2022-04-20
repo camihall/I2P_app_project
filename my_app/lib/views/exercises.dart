105ee69e5947eace137620a5b54e5346ec1babd2
ShapiroJoel <joelis@bu.edu>
2022-04-19 21:39:56 -0400

    Exercises done besides needing to click on questions to get to Qs page

--- my_app/lib/views/exercises.dart
+++ my_app/lib/views/exercises.dart
@@ -97,56 +97,115 @@ class exercises extends StatelessWidget {
 class exerciseAll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new exerciseBox("Exercise 1"),
-            new exerciseBox("Exercise 2"),
-          ]),
-      Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new exerciseBox("Exercise 3"),
-            new exerciseBox("Exercise 4"),
-          ]),
-      Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            new exerciseBox("Exercise 5"),
-            new exerciseBox("Exercise 6"),
-          ])
-    ]);
+    return Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [new exerciseBox()]);
   }
 }
 
-class exerciseBox extends StatelessWidget {
-  exerciseBox(this.exerciseName);
-  String exerciseName;
+class questionBox extends StatelessWidget {
+  final String qText;
+  questionBox({required String qText}) : this.qText = qText;
 
   Widget build(BuildContext context) {
-    return GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => exercises()),
-          );
-        },
-        child: Container(
-          height: MediaQuery.of(context).size.height * .25,
-          width: MediaQuery.of(context).size.width * .20,
-          color: Colors.transparent,
-          child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
-              child: new Center(
-                child: new Text(
-                  exerciseName,
-                  style: TextStyle(color: Colors.white, fontSize: 22),
-                  textAlign: TextAlign.center,
-                ),
-              )),
-        ));
+    return Container(
+        height: MediaQuery.of(context).size.height * .04,
+        width: MediaQuery.of(context).size.width * .70,
+        decoration: BoxDecoration(
+            border: Border(
+                bottom: BorderSide(color: Color(0xE7E7E7dd), width: .5))),
+        child: Text(qText));
+  }
+}
+
+final questions = [
+  questionBox(
+    qText: 'Question 1',
+  ),
+  questionBox(
+    qText: 'Question 2',
+  ),
+  questionBox(
+    qText: 'Question 3',
+  ),
+  questionBox(
+    qText: 'Question 4',
+  ),
+  questionBox(
+    qText: 'Question 5',
+  ),
+  questionBox(
+    qText: 'Question 6',
+  ),
+  questionBox(
+    qText: 'Question 7',
+  ),
+  questionBox(
+    qText: 'Question 8',
+  ),
+  questionBox(
+    qText: 'Question 9',
+  ),
+  questionBox(
+    qText: 'Question 10',
+  ),
+  questionBox(
+    qText: 'Question 11',
+  ),
+  questionBox(
+    qText: 'Question 12',
+  ),
+];
+
+class exerciseBox extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * .45,
+      width: MediaQuery.of(context).size.width * .70,
+      color: Colors.transparent,
+      child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          child: new Column(children: [
+            new Align(
+                alignment: Alignment.topCenter,
+                child: new Column(
+                  children: [
+                    new Text(
+                      "Exercises",
+                      style: TextStyle(color: Colors.black, fontSize: 22),
+                      textAlign: TextAlign.center,
+                    ),
+                    new Text(
+                      "Choose a question",
+                      style: TextStyle(color: Colors.black, fontSize: 18),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                )),
+            new Container(
+              height: MediaQuery.of(context).size.height * .37,
+              width: MediaQuery.of(context).size.width * .68,
+              child: new SingleChildScrollView(
+                  scrollDirection: Axis.vertical,
+                  child: new Align(
+                      alignment: Alignment.centerLeft,
+                      child: new Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: new Column(children: questions)))),
+            )
+          ]),
+          padding: EdgeInsets.all(5.0)),
+    );
   }
 }
