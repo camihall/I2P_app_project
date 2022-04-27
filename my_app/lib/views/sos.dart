00bf2bc4b645c1b9f8cf97a026a47d76273f4358
ShapiroJoel <joelis@bu.edu>
2022-04-27 17:20:46 -0400

    sos page starting to come together

--- my_app/lib/views/sos.dart
+++ my_app/lib/views/sos.dart
@@ -54,7 +54,7 @@ class SOSRoute extends StatelessWidget {
                                               .controlMenu,
                                         ),
                                       const SizedBox(width: 30),
-                                      const Text("Additional Resources",
+                                      const Text("Resources",
                                           style: TextStyle(
                                               color: Color(0xff0B3F24),
                                               fontWeight: FontWeight.bold,
@@ -87,74 +87,179 @@ class MySquares extends StatelessWidget {
     return Container(
         child: Padding(
       padding: EdgeInsets.only(bottom: 14),
-      child: Column(
-        children: [
-          InkWell(
-              onTap: () async {
-                final url = "https://beyondbarriersusa.org/";
-                if (await canLaunch(url)) {
-                  await launch(
-                    url,
-                  );
-                }
-              },
-              splashColor: Colors.white10,
-              child: Ink.image(
-                image: AssetImage("assets/images/beyondbarriers.png"),
-                height: MediaQuery.of(context).size.height * .45,
-                width: MediaQuery.of(context).size.width * .45,
-              )),
-          Text("Beyond Barriers"),
-          InkWell(
-              onTap: () async {
-                final url = "https://www.pnetworks.org/";
-                if (await canLaunch(url)) {
-                  await launch(
-                    url,
-                  );
-                }
-              },
-              splashColor: Colors.white10,
-              child: Ink.image(
-                image: AssetImage("assets/images/pnetworks.png"),
-                height: MediaQuery.of(context).size.height * .45,
-                width: MediaQuery.of(context).size.width * .45,
-              )),
-          Text("Parallel Networks"),
-          InkWell(
-              onTap: () async {
-                final url = "https://www.miraclecourt.com/";
-                if (await canLaunch(url)) {
-                  await launch(
-                    url,
-                  );
-                }
-              },
-              splashColor: Colors.white10,
-              child: Ink.image(
-                image: AssetImage("assets/images/mcourt.png"),
-                height: MediaQuery.of(context).size.height * .45,
-                width: MediaQuery.of(context).size.width * .45,
-              )),
-          Text("Miracle Court"),
-          InkWell(
-              onTap: () async {
-                final url = "http://www.savingcain.org/";
-                if (await canLaunch(url)) {
-                  await launch(
-                    url,
-                  );
-                }
-              },
-              splashColor: Colors.white10,
-              child: Ink.image(
-                image: AssetImage("assets/images/savingcain.jpeg"),
-                height: MediaQuery.of(context).size.height * .45,
-                width: MediaQuery.of(context).size.width * .45,
-              )),
-          Text("SavingCain.org"),
-        ],
-      ),
+      child: Column(children: [
+        Align(
+            alignment: Alignment.topLeft,
+            child: Text("Mental Health and Wellbeing",
+                style: TextStyle(fontSize: 18))),
+        Row(
+          children: [
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "https://www.miraclecourt.com/";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    splashColor: Colors.white10,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Ink.image(
+                      image: AssetImage("assets/images/mcourt.png"),
+                      height: MediaQuery.of(context).size.height * .15,
+                      width: MediaQuery.of(context).size.width * .15,
+                    )),
+                Text("Miracle Court"),
+              ],
+            ),
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "http://www.savingcain.org/";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    splashColor: Colors.white10,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Ink.image(
+                      image: AssetImage("assets/images/savingcain.jpeg"),
+                      height: MediaQuery.of(context).size.height * .15,
+                      width: MediaQuery.of(context).size.width * .15,
+                    )),
+                Text("SavingCain.org"),
+              ],
+            ),
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "https://www.headspace.com/";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    borderRadius: BorderRadius.circular(10),
+                    splashColor: Colors.white10,
+                    child: Ink.image(
+                      image: AssetImage("assets/images/headspace.png"),
+                      height: MediaQuery.of(context).size.height * .15,
+                      width: MediaQuery.of(context).size.width * .15,
+                    )),
+                Text("Headspace"),
+              ],
+            )
+          ],
+        ),
+        Align(
+            alignment: Alignment.topLeft,
+            child:
+                Text("Deradicalization NGOs", style: TextStyle(fontSize: 18))),
+        Row(
+          children: [
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "https://www.parents4peace.org/";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    splashColor: Colors.white10,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Ink.image(
+                      image: AssetImage("assets/images/p4p.png"),
+                      height: MediaQuery.of(context).size.height * .20,
+                      width: MediaQuery.of(context).size.width * .30,
+                    )),
+                Text("Parents for Peace"),
+              ],
+            ),
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "https://beyondbarriersusa.org/";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    splashColor: Colors.white10,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Ink.image(
+                      image: AssetImage("assets/images/beyondbar.png"),
+                      height: MediaQuery.of(context).size.height * .20,
+                      width: MediaQuery.of(context).size.width * .30,
+                    )),
+                Text("Beyond Barriers"),
+              ],
+            ),
+          ],
+        ),
+        Align(
+            alignment: Alignment.topLeft,
+            child: Text("Counseling & Therapy Directories",
+                style: TextStyle(fontSize: 18))),
+        Row(
+          children: [
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "https://www.psychologytoday.com/us";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    splashColor: Colors.white10,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Ink.image(
+                      image: AssetImage("assets/images/psychtoday.png"),
+                      height: MediaQuery.of(context).size.height * .20,
+                      width: MediaQuery.of(context).size.width * .25,
+                    )),
+                Text("Psychology Today"),
+              ],
+            ),
+            Column(
+              children: [
+                InkWell(
+                    onTap: () async {
+                      final url = "https://www.zocdoc.com/";
+                      if (await canLaunch(url)) {
+                        await launch(
+                          url,
+                        );
+                      }
+                    },
+                    splashColor: Colors.white10,
+                    borderRadius: BorderRadius.circular(10),
+                    child: Ink.image(
+                      image: AssetImage("assets/images/zocdoc.png"),
+                      height: MediaQuery.of(context).size.height * .20,
+                      width: MediaQuery.of(context).size.width * .25,
+                    )),
+                Text("ZocDoc"),
+              ],
+            ),
+          ],
+        ),
+      ]),
     ));
   }
 }
